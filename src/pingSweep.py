7bb4671da08aeb468b8db84f7f6e5c0665e0c0fe
linx3273 <ayush96.gupta@gmail.com>
2022-05-01 11:56:56 +0530

    made few variables in class private

--- src/pingSweep.py
+++ src/pingSweep.py
@@ -10,11 +10,11 @@ class pingSweep:
         # getting the network range for currently connected network
         self.addr = ipaddress.ip_network(get_if_addr(conf.iface)+'/'+'255.255.255.0',strict=False)
 
-        self.respCount = 0
-        self.blockCount = 0
-        self.checkCount = 0
-        self.blocks = []
-        self.active = []
+        self.__respCount = 0
+        self.__blockCount = 0
+        self.__checkCount = 0
+        self.__blocks = []
+        self.__active = []
         
         
     def sweep(self):
@@ -38,35 +38,35 @@ class pingSweep:
                 pass
             elif (int(resp.getlayer(ICMP).type)==3 and int(resp.getlayer(ICMP).code in [1,2,3,9,10,13])):
                 self.block.append(host)
-                self.blockCount+=1
+                self.__blockCount+=1
             else:
                 self.active.append(host)
-                self.respCount+=1
+                self.__respCount+=1
             
-            self.checkCount += 1
-            if self.stop==self.checkCount:
+            self.__checkCount += 1
+            if self.stop==self.__checkCount:
                 break
         
 
 
     def showResults(self):
         print("\n\n=====================================")   
-        print("Total checks = " + Fore.GREEN + f"{self.checkCount}")
+        print("Total checks = " + Fore.GREEN + f"{self.__checkCount}")
         self.showActive()
         self.showBlock()
         print("=====================================")   
 
     def showActive(self):
-        print("\nActive hosts = " + Fore.GREEN + f"{self.respCount}")
+        print("\nActive hosts = " + Fore.GREEN + f"{self.__respCount}")
         print("---------------------")        
         for i in self.active:
             print(i)
         print("---------------------")
 
     def showBlock(self):
-        print(f"\nActive but blocking hosts = "+ Fore.GREEN +f"{self.blockCount}")
+        print(f"\nActive but blocking hosts = "+ Fore.GREEN +f"{self.__blockCount}")
         print("---------------------")        
-        for i in self.blocks:
+        for i in self.__blocks:
             print(i)
         print("---------------------")
 
--- src/portScanner.py
+++ src/portScanner.py
@@ -6,67 +6,67 @@ from colorama import Fore
 class portScanner:
     def __init__(self):
         colorama.init(autoreset=True)
-        self.ip = get_if_addr(conf.iface)
-        self.srcPort = 3000
-        self.destPort = list(range(0,65534))
+        self.__ip = get_if_addr(conf.iface)
+        self.__srcPort = 3000
+        self.__destPort = list(range(0,65534))
         # 22 - SSH, Secure logins, filetransfers and port forwarding
         # 23 - Telnet protocol - unencrypted text comms
         # 80 - HTTP uses TCP v1.x and 2
         # 443 - HTTPS uses TCP v1.x and 2
         # 3389 - Microsoft Terminal Server (RDP) official registered as Windows Based Terminal
-        self.checkCount = 0
-        self.activeCount = 0
-        self.unusedCount = 0
-        self.filteredCount = 0
-        self.unused = []
-        self.active = []
-        self.filtered = []
+        self.__checkCount = 0
+        self.__activeCount = 0
+        self.__unusedCount = 0
+        self.__filteredCount = 0
+        self.__unused = []
+        self.__activee = []
+        self.__filtered = []
     
     def scanport(self,ext=0):
         if ext==1:
-            self.ip = input("Enter IP address: ")
+            self.__ip = input("Enter IP address: ")
         self.start = int(input("Enter start value: "))
         self.stop = int(input("Enter stop value: "))
         
-        for dstPort in self.destPort:
+        for dstPort in self.__destPort:
             dstPort = dstPort + self.start
             # send SYN using port 3000 to dstPort
             resp = sr1(
-                IP(dst=self.ip)/TCP(sport=self.srcPort,dport=dstPort,flags='S'),
+                IP(dst=self.__ip)/TCP(sport=self.__srcPort,dport=dstPort,flags='S'),
                 timeout=1,
                 verbose=0,
             )
 
             if resp is None:
-                self.unused.append(dstPort)
-                self.unusedCount+=1
+                self.__unused.append(dstPort)
+                self.__unusedCount+=1
             
             elif (resp.haslayer(TCP)):
                 # to close the connection if dstPort responds
                 send_rst = sr1(
-                    IP(dst=self.ip)/TCP(sport=self.srcPort,dport=dstPort,flags='R'),
+                    IP(dst=self.__ip)/TCP(sport=self.__srcPort,dport=dstPort,flags='R'),
                     timeout=1,
                     verbose=0,
                 )
 
-                self.activeCount+=1
-                self.active.append(dstPort)
+                self.__activeCount+=1
+                self.__activee.append(dstPort)
 
             elif (resp.haslayer(ICMP)):
                 if( int(resp.getlayer(ICMP).type) == 3 and int(resp.getlayer(ICMP).code) in [1,2,3,9,10,13]):
-                    self.filteredCount+=1
-                    self.filtered.append(dstPort)
+                    self.__filteredCount+=1
+                    self.__filtered.append(dstPort)
                 
 
             print(dstPort)
-            self.checkCount+=1
-            if self.stop==self.checkCount:
+            self.__checkCount+=1
+            if self.stop==self.__checkCount:
                 break
             
 
     def showResults(self,ext=0):
         print("\n\n=====================================")   
-        print("Total checks = " + Fore.GREEN + f"{self.checkCount}")
+        print("Total checks = " + Fore.GREEN + f"{self.__checkCount}")
         self.showActive()
         self.showFiltered()
         if ext==0:
@@ -74,23 +74,23 @@ class portScanner:
         print("=====================================")  
 
     def showActive(self):
-        print("\nActive ports = " + Fore.GREEN + f"{self.activeCount}")
+        print("\nActive ports = " + Fore.GREEN + f"{self.__activeCount}")
         print("---------------------")        
-        for i in self.active:
+        for i in self.__activee:
             print(i)
         print("---------------------")
 
     def showUnused(self):
-        print(f"\nUnused ports = "+ Fore.GREEN +f"{self.unusedCount}")
+        print(f"\nUnused ports = "+ Fore.GREEN +f"{self.__unusedCount}")
         print("---------------------")        
-        for i in self.unused:
+        for i in self.__unused:
             print(i)
         print("---------------------")
 
     def showFiltered(self):
-        print(f"\nFiltered ports = "+ Fore.GREEN +f"{self.filteredCount}")
+        print(f"\nFiltered ports = "+ Fore.GREEN +f"{self.__filteredCount}")
         print("---------------------")        
-        for i in self.filtered:
+        for i in self.__filtered:
             print(i)
         print("---------------------")      
 
